66daa99eb11da0ffac76d0f96f471982eac6ab76
JackXu0941 <chengkai4904@163.com>
2025-09-24 17:48:22 +0800

    exercise_2.sol

--- 20250811_第二阶段_solidity任务2/exercise_2.sol
+++ 20250811_第二阶段_solidity任务2/exercise_2.sol
@@ -1,29 +1,35 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721, ERC721URIStorage {
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
-
-    constructor() ERC721("MyNFT", "MNFT") {}
-
-    function mintNFT(address recipient, string memory tokenURI) public returns (uint256) {
-        _tokenIds.increment();
-        uint256 newTokenId = _tokenIds.current();
-        _mint(recipient, newTokenId);
-        _setTokenURI(newTokenId, tokenURI);
-        return newTokenId;
+contract NFTDemo is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+    
+    constructor() ERC721("MyNFT", "MNFT") Ownable(msg.sender) {}
+    
+    function mintNFT(address _recipient, string memory _tokenURI) public onlyOwner returns (uint256) {
+        _tokenIdCounter++;
+        uint256 tokenId = _tokenIdCounter;
+        
+        _safeMint(_recipient, tokenId);
+        _setTokenURI(tokenId, _tokenURI);
+        
+        return tokenId;
     }
-
-    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721URIStorage) returns (bool) {
-        return super.supportsInterface(interfaceId);
+    
+    function getCurrentTokenId() public view returns (uint256) {
+        return _tokenIdCounter;
     }
-
-    function tokenURI(uint256 tokenId) public view virtual override(ERC721, ERC721URIStorage) returns (string memory) {
+    
+    // 重写必要的函数以支持ERC721URIStorage
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
+    }
+    
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721URIStorage) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
